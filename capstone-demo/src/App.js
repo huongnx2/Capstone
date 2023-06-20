5ddc55d07ac73fdfa36dc1ae099fc7a85e846c0a
EC2 Default User <ec2-user@ip-172-31-70-164.ec2.internal>
2023-06-20 08:10:46 +0000

    k8s

--- capstone-demo/src/App.js
+++ capstone-demo/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Reviewers, this is my CAPSTONE demo
         </p>
         <a
           className="App-link"
